918503fc455d0c3865f109b5195100a3745a7995
Giovanni Noisy <gnoisy77@gmail.com>
2020-07-15 15:42:02 -0700

    set up inquirer prompt for manager info in app.js

--- app.js
+++ app.js
@@ -10,7 +10,32 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
+const teamMembers = [];
+inquirer.prompt([
+    {
+        type: "input",
+        message: "Enter manager's name: ",
+        name: "name",
+    },
+    {
+        type: "input",
+        message: "Enter manager's id: ",
+        name: "id",
+    },
+    {
+        type: "input",
+        message: "Enter manager's email: ",
+        name: "email",
+    },
+    {
+        type: "input",
+        message: "Enter manager's office number: ",
+        name: "officeNumber",
+    }
+])
+    .then(answers => {
+        teamMembers.push(new Manager(answers.name, answers.id, answers.email, answers.officeNumber));
+    })
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
